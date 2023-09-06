a1c879a31898c4bd7d32fac4e2c481c62e0b9317
Parth Malavia <parth.malavia@crestdatasys.com>
2023-09-06 10:09:39 +0000

    customized view

--- custom_vizualisation/test_viz.js
+++ custom_vizualisation/test_viz.js
@@ -1,79 +1,70 @@
+// Use Looker's Visualization API
 looker.plugins.visualizations.add({
-    // Id and Label are legacy properties that no longer have any function besides documenting
-    // what the visualization used to have. The properties are now set via the manifest
-    // form within the admin/visualizations page of Looker
-    id: "hello_world",
-    label: "Hello World",
-    options: {
-        font_size: {
-            type: "string",
-            label: "Font Size",
-            values: [
-                { "Large": "large" },
-                { "Small": "small" }
-            ],
-            display: "radio",
-            default: "large"
-        }
-    },
-    // Set up the initial state of the visualization
-    create: function (element, config) {
+    id: 'display_count',
+    label: 'Display Count',
+    create: function(element, config) {
+      // Create a container for the count value
+      this.container = element.appendChild(document.createElement("div"));
+      this.container.setAttribute("id", "count-line-container");
 
-        // Insert a <style> tag with some styles we'll use later.
-        element.innerHTML = `
-        <style>
-          .hello-world-vis {
-            /* Vertical centering */
-            height: 100%;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            text-align: center;
-          }
-          .hello-world-text-large {
-            font-size: 72px;
-          }
-          .hello-world-text-small {
-            font-size: 18px;
-          }
-        </style>
+      // Applying Styling to the container
+      this.container.style.fontWeight = "bold";
+      this.container.style.textAlign = "center";
+      this.container.style.padding = "25px";
+      this.container.style.display = "flex";
+      this.container.style.flexDirection = "column";
+      this.container.style.alignItems = "center";
+
+      // Create a container for the text line
+      this.textContainer = element.appendChild(document.createElement("div"));
+      this.textContainer.setAttribute("id", "text-line-container");
+      this.textContainer.style.fontSize = "16px";
+      this.textContainer.style.textAlign = "center";
+      this.textContainer.style.padding = "5px";
+      this.textContainer.style.fontFamily = "Arial";
+    },
+
+    updateAsync: function(data, element, config, queryResponse, details, done) {
+      // Calculate the count value from the data
+      const count = data.length;
+
+      let list=[]
+      let list1=[]
+      for (var i of queryResponse.fields.measures) {
+        var th = document.createElement('th');
+        list.push(i.name);
+      }
+      data.forEach(function (row) {
+        Object.keys(row).forEach(function (key) {
+          list1.push(row[key].value);
+        });
+      });
+      // console.log("list:",list)
+      // console.log("list1:",list1)
+
+      // Calculate the percentage value based on the available count
+      const estimatedTotalItems = 100;
+      const percentage = ((count / estimatedTotalItems) * 100).toFixed(2);
+      const previousPercentage = 75;
+
+      const percentageChange = percentage - previousPercentage;
+      const arrowIcon = percentageChange > 0 ? '➚' : '➘';
+
+      // Display the count and percentage value in the container
+      this.container.innerHTML = `
+        <div style="display: flex; align-items: center;">
+          <div style="font-size: 60px;">${list1[1]}</div>
+          <div style="display: flex; flex-direction: column; align-items: flex-start;">
+            <div style="font-size: 30px;">${arrowIcon}</div>
+            <div style="font-size: 20px; text-align: right;">${percentage}%</div>
+          </div>
+        </div>
       `;
 
-        // Create a container element to let us center the text.
-        var container = element.appendChild(document.createElement("div"));
-        container.className = "hello-world-vis";
+      // Display the text line below the count value
+      this.textContainer.textContent = "Today vs Yesterday";
 
-        // Create an element to contain the text.
-        this._textElement = container.appendChild(document.createElement("div"));
-
-    },
-    // Render in response to the data or settings changing
-    updateAsync: function (data, element, config, queryResponse, details, done) {
-
-        // Clear any errors from previous updates
-        this.clearErrors();
-
-        // Throw some errors and exit if the shape of the data isn't what this chart needs
-        if (queryResponse.fields.dimensions.length == 0) {
-            this.addError({ title: "No Dimensions", message: "This chart requires dimensions." });
-            return;
-        }
-
-        // Grab the first cell of the data
-        var firstRow = data[0];
-        var firstCell = firstRow[queryResponse.fields.dimensions[0].name];
-
-        // Insert the data into the page
-        this._textElement.innerHTML = LookerCharts.Utils.htmlForCell(firstCell);
-
-        // Set the size to the user-selected size
-        if (config.font_size == "small") {
-            this._textElement.className = "hello-world-text-small";
-        } else {
-            this._textElement.className = "hello-world-text-large";
-        }
-
-        // We are done rendering! Let Looker know.
-        done()
+      // Signal the completion of rendering
+      done();
     }
-});
+  });
