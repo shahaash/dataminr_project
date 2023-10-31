5aea8ac082cdef781d5951686e1aa4807dc1cf55
Monil Shah <monil.shah@crestdatasys.com>
2023-10-31 12:44:27 +0000

    Removed decimal from cyber viz

--- custom_vizualisation/cyber_viz.js
+++ custom_vizualisation/cyber_viz.js
@@ -6,7 +6,7 @@ looker.plugins.visualizations.add({
       // Create a container for the count value
       this.container = element.appendChild(document.createElement("div"));
       this.container.setAttribute("id", "count-line-container");
-  
+
       // Applying Styling to the container
       this.container.style.fontWeight = "bold";
       this.container.style.textAlign = "center";
@@ -14,7 +14,7 @@ looker.plugins.visualizations.add({
       this.container.style.display = "flex";
       this.container.style.flexDirection = "column";
       this.container.style.alignItems = "center";
-  
+
       // Create a container for the text line
       this.textContainer = element.appendChild(document.createElement("div"));
       this.textContainer.setAttribute("id", "text-line-container");
@@ -22,8 +22,8 @@ looker.plugins.visualizations.add({
       this.textContainer.style.textAlign = "center";
       this.textContainer.style.padding = "5px";
       this.textContainer.style.fontFamily = "Arial";
-  
-  
+
+
      // Create a container element for your chart
       this.parentNode = document.createElement("div");
       this.parentNode.style.display = "flex";
@@ -33,9 +33,9 @@ looker.plugins.visualizations.add({
       this.chart_container.className = "line-chart-container";
       this.parentNode.appendChild(this.chart_container);
       element.appendChild(this.parentNode);
-  
+
     },
-  
+
     updateAsync: function(data, element, config, queryResponse, details, done) {
       var threatcolumn = queryResponse.fields.measure_like[0].name
       // Calculate the count value from the data
@@ -55,16 +55,16 @@ looker.plugins.visualizations.add({
       const estimatedTotalItems = 100;
       const threat_count = count ? data[0][threatcolumn].value:0;
       var threat1_count = 0;
-      if (count != 1 && count != 0) {
+      if (count != 1) {
           threat1_count = data[1][threatcolumn].value
       }
       const threat_count_difference = count ? threat_count - threat1_count:0
       var percentage = 0
-      if (count != 1 && count != 0) {
-          percentage = count ? ((threat_count_difference / threat1_count) * estimatedTotalItems).toFixed(2):0;
+      if (count != 1) {
+          percentage = count ? ((threat_count_difference / threat1_count) * estimatedTotalItems):0;
       }
       const arrowIcon = percentage > 0 ? '➚' : '➘';
-  
+
       var color;
       if (percentage <= 0) {
         color = 'green';
@@ -72,7 +72,7 @@ looker.plugins.visualizations.add({
       else {
         color = 'red';
       }
-  
+
       // Display the count and percentage value in the container
       this.container.innerHTML = `
         <div style="display: flex; align-items: center;">
@@ -83,26 +83,26 @@ looker.plugins.visualizations.add({
           </div>
         </div>
       `;
-  
+
       // Display the text line below the count value
       this.textContainer.textContent = "Today vs Yesterday";
       this.textContainer.style.fontSize = "12px";
       this.textContainer.style.fontFamily = "Arial, Helvetica, sans-serif";
-  
+
       // Extract data from Looker's query response
       var labels = [];
       var datasets = [];
-  
+
       var xField = queryResponse.fields.dimension_like[0].name;
       var yField = queryResponse.fields.measure_like[0].name;
-  
+
       // Populate labels and datasets based on your data model
       data.forEach(function(row) {
         datasets.push(row[yField].value ? row[yField].value : 0);
         labels.push(row[xField].value);
       });
-  
-  
+
+
       // Initialize a Chart.js instance
         var ctx = this.chart_container;
         if (this.chart) {
@@ -139,13 +139,13 @@ looker.plugins.visualizations.add({
             }
           },
         });
-  
+
       this.chart.canvas.style.height = '50px';
       this.chart.canvas.style.width = '200px';
       // Update the chart
       this.chart.update();
-  
+
       // Signal the completion of rendering
       done();
     }
-  });  
+  });
